3d285a23b10b5f17c1f42662e316c2dac014ce2c
Barnaby Keene <hello@southcla.ws>
2019-07-07 20:50:36 +0100

    add seo stuf

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import App, { Container } from "next/app";
 import Router from "next/router";
 import withGA from "next-ga";
+import DefaultSeo from "next-seo";
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -19,6 +20,32 @@ class MyApp extends App {
 
     return (
       <Container>
+        <DefaultSeo
+          config={{
+            title: "Digital Banks List",
+            description: "A comparison of digital banks and their features.",
+            canonical: "https://www.banks.digital/",
+            openGraph: {
+              type: "website",
+              locale: "en_GB",
+              url: "https://www.banks.digital/",
+              site_name: "Digital Banks List",
+              images: [
+                {
+                  url: "/static/large.png",
+                  width: 866,
+                  height: 296,
+                  alt: "Digital Banking Banner"
+                }
+              ]
+            },
+            twitter: {
+              handle: "@Southclaws",
+              site: "@Southclaws",
+              cardType: "summary_large_image"
+            }
+          }}
+        />
         <Component {...pageProps} />
       </Container>
     );
