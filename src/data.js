48972350bd35d94a06ccfa924fd6d6799b5691ee
Barnaby Keene <accounts@southcla.ws>
2019-07-26 18:52:47 +0100

    working on using better api

--- src/data.js
+++ src/data.js
@@ -1,4 +1,6 @@
 import fetch from "isomorphic-unfetch";
+import * as GoogleSheets from "google-drive-sheets";
+import { promisify } from "util";
 
 const SHEET_ID = "1b081m9EBaib-LQaYmYC3oMY0Z-yNY9SI_JwqIC50fpI";
 const PAGE = "1";
@@ -21,26 +23,36 @@ const prettify = text => {
 };
 
 export const getData = async () => {
-  const raw = await getSheet(SHEET_ID, PAGE);
-  const entries = raw.feed.entry;
+  let sheet = new GoogleSheets(SHEET_ID);
 
-  let headers = [];
-  let data = {};
-  let current = "";
-  entries.forEach(e => {
-    if (e["gs$cell"].row == 1 && e["gs$cell"].col != 1) {
-      data[e["gs$cell"].col] = {
-        Name: e.content["$t"]
-      };
-    } else {
-      if (e["gs$cell"].col == 1) {
-        current = e.content["$t"];
-        headers.push(current);
-      } else {
-        data[e["gs$cell"].col][current] = prettify(e.content["$t"]);
-      }
-    }
-  });
+  let getRows = promisify(sheet.getRows);
 
-  return [headers, Object.keys(data).map(v => data[v])];
+  let data = await getRows(1);
+
+  console.log(data);
+
+  return;
+
+  // const raw = await getSheet(SHEET_ID, PAGE);
+  // const entries = raw.feed.entry;
+
+  // let headers = [];
+  // let data = {};
+  // let current = "";
+  // entries.forEach(e => {
+  //   if (e["gs$cell"].row == 1 && e["gs$cell"].col != 1) {
+  //     data[e["gs$cell"].col] = {
+  //       Name: e.content["$t"]
+  //     };
+  //   } else {
+  //     if (e["gs$cell"].col == 1) {
+  //       current = e.content["$t"];
+  //       headers.push(current);
+  //     } else {
+  //       data[e["gs$cell"].col][current] = prettify(e.content["$t"]);
+  //     }
+  //   }
+  // });
+
+  // return [headers, Object.keys(data).map(v => data[v])];
 };
